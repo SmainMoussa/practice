b269bdd4a138851c3f95656c6acb5350cd16fbb4
Smain <moussa59124@gmail.com>
2021-10-30 19:02:53 +0200

    practice_2 added

---
+++ js/practice_2.js
@@ -0,0 +1,11 @@
+
+var randchiffre = parseInt(Math.random() * 10)
+var chiffre = prompt("entre un chiffre")
+
+console.log(randchiffre)
+if (chiffre === randchiffre) {
+    alert("GJ u guessed it")
+} else {
+    alert("u failed")
+
+}
